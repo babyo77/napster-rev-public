aaf18977687fe4fb2cc8213c1f24a4060d9cd1b3
babyo77 <onlybabyyo7@gmail.com>
2024-04-05 21:39:41 +0530

    Update sample.appwriteConfig.ts

--- src/appwrite/sample.appwriteConfig.ts
+++ src/appwrite/sample.appwriteConfig.ts
@@ -5,14 +5,24 @@ client.setEndpoint("https://cloud.appwrite.io/v1").setProject("YOUR PROJECT");
 
 export const DATABASE_ID = "YOUR ID";
 export const PLAYLIST_COLLECTION_ID = "YOUR ID";
+export const ALBUM_COLLECTION_ID = "YOUR ID";
 export const LISTEN_NOW_COLLECTION_ID = "YOUR ID";
-export const TRENDING_COLLECTION_ID = "YOUR ID";
-export const LIKE_SONG = "";
-export const INSIGHTS = "";
+export const MOST_PLAYED = "YOUR ID";
+export const LIKE_SONG = "YOUR ID";
+export const FAV_ARTIST = "YOUR ID";
+export const INSIGHTS = "YOUR ID";
 export const ARTIST_INSIGHTS = "YOUR ID";
 export const PLAYLIST_INSIGHTS = "YOUR ID";
+export const ALBUM_INSIGHTS = "YOUR ID";
 export const NEW_USER = "YOUR ID";
-
+export const UPDATES = "YOUR ID";
+export const SPONSORS = "YOUR ID";
+export const LAST_PLAYED = "YOUR ID";
+export const ADD_TO_LIBRARY = "YOUR ID";
+export const BUCKET = "YOUR ID";
+export const STORAGE = "YOUR ID";
+export const storage = new Storage(client);
+export const LYRICS = "YOUR ID";
 export const db = new Databases(client);
 
 export { ID } from "appwrite";
